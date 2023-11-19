e6f89aea4270c773c0f4cefc7fb71e8e463c4b24
alathea23 <135459921+alathea23@users.noreply.github.com>
2023-11-19 03:19:42 -0800

    rewriting as constructor functions

--- index.js
+++ index.js
@@ -1,38 +1,50 @@
-const inquirer = require('inquirer')
-const fs = require('fs');
-const markdown = require('./utils/generateMarkdown')
+const inquirer = require("inquirer");
+const fs = require("fs");
+const shapes = require("./lib/shapes");
 
 //let answers = []
 
 //Creating an array of questions for user input
-const questions = inquirer
-    .prompt([
-        {
-            type: 'list',
-            message: 'What license are you using?',
-            choices: ['MIT', "Apache", "Eclipse", "GPLv3", "GPL", "Mozilla", "Perl", "N/A"],
-            name: 'license'
-        },
-        {
-            type: 'input',
-            message: 'What is your project title?',
-            name: 'title'
-        },
-        {
-            type: 'input',
-            message: 'Give a brief project description',
-            name: 'description'
-        },
-    ])
+const questions = inquirer.prompt([
+  {
+    type: "list",
+    message: "What shape would you like",
+    choices: ["Circle", "Triangle", "Square"],
+    name: "shape",
+  },
+  {
+    type: "input",
+    message: "What color would you like your logo background?",
+    name: "color",
+  },
+  {
+    type: "input",
+    maxLength: 3,
+    message: "What script would you like on your logo",
+    name: "text",
+  },
+  {
+    type: "input",
+    message: "What color would you like your text?",
+    name: "textcolor",
+  },
+]);
 
+function writeFile(answers) {
+  fileName = "logo.svg";
+  (writeText = shapes(answers)),
+    //console to test code
+    console.log(logoParams);
 
+  fs.writeFile(fileName, logoParams, (err) =>
+    err ? console.error(err) : console.log("Generated logo.svg")
+  );
+}
 
-        async function init() {
-            const port = process.env.PORT || 3001
-            await questions
-            writeFile(answers)
-        }
-        
-        // Function call to initialize app
-        init();
-        
+async function init() {
+  await questions;
+  writeFile(answers);
+}
+
+// Function call to initialize app
+init();
